b257172587e86fd88f2575391d2ed2c8eb39d054
Nagaraju <raj.vorugan@gmail.com>
2019-12-27 13:14:05 -0600

    update to db logic

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
-
-mongoose.connect(process.env.DB_CONNECTION_URL,{
+const DB_CLOUD_URL = DB_CONNECTION_URL='mongodb+srv://raj_vorugan:Hariom143@orgcluster-0wpoe.gcp.mongodb.net/company?retryWrites=true&w=majority'
+const DB_CONNECTION_URL = process.env.DB_CONNECTION_URL || DB_CLOUD_URL
+mongoose.connect(DB_CONNECTION_URL,{
     useNewUrlParser:true,
     useCreateIndex:true,
     useFindAndModify:false,
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const app = require('./app.js')
-const port = process.env.PORT
+const port = process.env.PORT || 3000
 
 app.listen(port,()=>{
     console.log('Server is up and running on port ',port)
