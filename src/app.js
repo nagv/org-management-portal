33ccd47b709076cdab9619d20c70169859562502
Nagaraju <raj.vorugan@gmail.com>
2019-12-28 13:10:44 -0600

    cors changes

--- src/app.js
+++ src/app.js
@@ -1,9 +1,16 @@
 const express = require('express')
+const cors = require('cors')
 const mongoose = require('mongoose')
 require('./db/mongoose')
 const employeeRouter = require('./routers/employee_router')
 
 const app = express()
+
+// Enable cors for external application
+app.use(cors({
+    origin: 'http://localhost:4200'
+  }))
+
 app.use(express.json())
 // Load employee resource router
 app.use(employeeRouter)
